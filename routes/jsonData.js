640559a5496f2d9f75da951818f07dfc0e58b63d
misupopo <kokorononakaniitumo@yahoo.co.jp>
2017-03-31 16:40:27 +0900

    微修正

--- routes/jsonData.js
+++ routes/jsonData.js
@@ -6,15 +6,15 @@ router.get('/', function (req, res, next) {
     res.json([
         {
             name: 'test1',
-            url: 'http://160.16.201.215:9000/testImg1.png'
+            url: 'http://160.16.201.215:9001/testImg1.png'
         },
         {
             name: 'test2',
-            url: 'http://160.16.201.215:9000/testImg2.png'
+            url: 'http://160.16.201.215:9001/testImg2.png'
         },
         {
             name: 'test3',
-            url: 'http://160.16.201.215:9000/testImg3.png'
+            url: 'http://160.16.201.215:9001/testImg3.png'
         }
     ]);
 });
