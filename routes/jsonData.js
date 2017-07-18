8ad27f16a75f4bde936d1ab0ba60fc77b3e10d9e
misupopo <kokorononakaniitumo@yahoo.co.jp>
2017-07-18 16:35:01 +0900

    jsonファイルの修正

--- routes/jsonData.js
+++ routes/jsonData.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.json([
         {
             name: 'test1',
-            url: 'http://160.16.201.215:9001/testImg1.png'
+            url: 'http://160.16.201.215:9001/test/testImg1.png'
         },
         {
             name: 'test2',
-            url: 'http://160.16.201.215:9001/testImg2.png'
+            url: 'http://160.16.201.215:9001/test/testImg2.png'
         },
         {
             name: 'test3',
-            url: 'http://160.16.201.215:9001/testImg3.png'
+            url: 'http://160.16.201.215:9001/test/testImg3.png'
         }
     ]);
 });
