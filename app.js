d0811d2b589feb2ebeb32f3f612fa4ddeb2bfc62
misupopo <kokorononakaniitumo@yahoo.co.jp>
2017-07-18 15:50:27 +0900

    directory の場所を修正

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(path.resolve('../bookGetter/frontTest'))));
 app.use(express.static(path.join(path.resolve('../bookGetter/frontTest/img/page/target'))));
 
 app.use('/', index);
