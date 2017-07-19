0f88c9613ace8ba12be67cc7f43461565341a299
misupopo <kokorononakaniitumo@yahoo.co.jp>
2017-07-19 19:09:49 +0900

    idを追加

--- routes/jsonData.js
+++ routes/jsonData.js
@@ -5,14 +5,17 @@ const router = express.Router();
 router.get('/', function (req, res) {
     res.json([
         {
+            id: 1,
             name: 'test1',
             url: 'http://160.16.201.215:9001/test/testImg1.png'
         },
         {
+            id: 2,
             name: 'test2',
             url: 'http://160.16.201.215:9001/test/testImg2.png'
         },
         {
+            id: 3,
             name: 'test3',
             url: 'http://160.16.201.215:9001/test/testImg3.png'
         }
